eeb20de10298ce6f4726f04bacb71f07cedb4011
fpral <fpral@jahia.com>
2022-11-18 11:43:59 +0100

    Updated definition + added custom event picker config and override

--- src/main/resources/javascript/apps/registerExtensions.js
+++ src/main/resources/javascript/apps/registerExtensions.js
@@ -1,34 +1,97 @@
 window.jahia.i18n.loadNamespaces('components-features-test');
 
-window.jahia.uiExtender.registry.add('callback', 'components-features-testExample', {
-    targets: ['jahiaApp-init:60'],
-    callback: function () {
-        window.jahia.uiExtender.registry.add('adminRoute', 'components-features-testExample', {
-            targets: ['administration-sites:999', 'components-features-testaccordion'],
-            label: 'components-features-test:label.settings.title',
-            icon: window.jahia.moonstone.toIconComponent('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6V5A2 2 0 0 0 17 3H15A2 2 0 0 0 13 5V6H11V5A2 2 0 0 0 9 3H7A2 2 0 0 0 5 5V6H3V20H21V6M19 18H5V8H19Z" /></svg>'),
-            isSelectable: true,
-            requireModuleInstalledOnSite: 'components-features-test',
-            iframeUrl: window.contextJsParameters.contextPath + '/cms/editframe/default/$lang/sites/$site-key.components-features-test.html.ajax'
-        });
+window.jahia.uiExtender.registry.add('callback', 'customEventPickerRegistration', {
+    //Content-Editor initialisation callback as a priority of 2
+    targets: ['jahiaApp-init:33'],
+    callback: () => {
+        const registry = window.jahia.uiExtender.registry;
 
-        window.jahia.uiExtender.registry.add('action', 'components-features-testExample', {
-            buttonIcon: window.jahia.moonstone.toIconComponent('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6V5A2 2 0 0 0 17 3H15A2 2 0 0 0 13 5V6H11V5A2 2 0 0 0 9 3H7A2 2 0 0 0 5 5V6H3V20H21V6M19 18H5V8H19Z" /></svg>'),
-            buttonLabel: 'components-features-test:label.action.title',
-            targets: ['contentActions:999'],
-            onClick: context => {
-                window.open('https://github.com/Jahia/app-shell/blob/master/docs/declare-new-module.md', "_blank");
-            }
-        });
-
-        window.jahia.uiExtender.registry.add('accordionItem', 'components-features-testApps_Example', window.jahia.uiExtender.registry.get('accordionItem', 'renderDefaultApps'), {
-            targets: ['jcontent:998'],
-            label: 'components-features-test:label.appsAccordion.title',
-            icon: window.jahia.moonstone.toIconComponent('<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6V5A2 2 0 0 0 17 3H15A2 2 0 0 0 13 5V6H11V5A2 2 0 0 0 9 3H7A2 2 0 0 0 5 5V6H3V20H21V6M19 18H5V8H19Z" /></svg>'),
-            appsTarget: 'components-features-testaccordion',
-            isEnabled: function(siteKey) {
-                return siteKey !== 'systemsite'
+        registry.add('pickerConfiguration', 'custom-event-picker', {
+            pickerInput: {
+                emptyLabel: 'components-features-test:label.picker.empty',
+                notFoundLabel: 'components-features-test:label.picker.notFound',
+            },
+            pickerDialog: {
+                view: 'List',
+                dialogTitle: 'components-features-test:label.picker.title',
+                displayTree: true,
+                displaySiteSwitcher: true,
+                displaySearch: true
+            },
+            searchContentType: 'jnt:event',
+            selectableTypesTable: ['jnt:event'],
+            accordions: ['picker-pages', 'picker-content-folders'],
+            accordionItem: {
+                "picker-pages": {
+                    tableConfig: {
+                        fragments: [{
+                            gql: jahia.graphqlTag('fragment MyProp on JCRNode { eventsType: property(name: "eventsType") { value:choicelistValue(renderer:"resourceBundle", language: $language) }, startDate: property(name: "startDate") { value }, endDate: property(name: "endDate") { value }}'),
+                            applyFor: 'node'
+                        }],
+                        columns: ["publicationStatus", "name", {
+                            id: 'event-type',
+                            accessor: row => row.eventsType && row.eventsType.value,
+                            label: 'components-features-test:label.picker.type',
+                            sortable: true,
+                            property: 'eventsType.value'
+                        },
+                            {
+                                id: 'start-date',
+                                accessor: row => row.startDate && new Date(row.startDate.value).toLocaleDateString(),
+                                label: 'components-features-test:label.picker.startDate',
+                                width: '150px',
+                                sortable: true,
+                                property: 'startDate.value'
+                            },
+                            {
+                                id: 'end-date',
+                                accessor: row => row.endDate && new Date(row.endDate.value).toLocaleDateString(),
+                                label: 'components-features-test:label.picker.endDate',
+                                width: '150px',
+                                sortable: true,
+                                property: 'endDate.value'
+                            }]
+                    },
+                    treeConfig: {
+                        hideRoot: true
+                    }
+                },
+                "picker-content-folders": {
+                    tableConfig: {
+                        fragments: [{
+                            gql: jahia.graphqlTag('fragment MyProp on JCRNode { eventsType: property(name: "eventsType") { value:choicelistValue(renderer:"resourceBundle", language: $language) }, startDate: property(name: "startDate") { value }, endDate: property(name: "endDate") { value }}'),
+                            applyFor: 'node'
+                        }],
+                        columns: ["publicationStatus", "name", {
+                            id: 'event-type',
+                            accessor: row => row.eventsType && row.eventsType.value,
+                            label: 'components-features-test:label.picker.type',
+                            sortable: true,
+                            property: 'eventsType.value'
+                        },
+                            {
+                                id: 'start-date',
+                                accessor: row => row.startDate && new Date(row.startDate.value).toLocaleDateString(),
+                                label: 'components-features-test:label.picker.startDate',
+                                width: '150px',
+                                sortable: true,
+                                property: 'startDate.value'
+                            },
+                            {
+                                id: 'end-date',
+                                accessor: row => row.endDate && new Date(row.endDate.value).toLocaleDateString(),
+                                label: 'components-features-test:label.picker.endDate',
+                                width: '150px',
+                                sortable: true,
+                                property: 'endDate.value'
+                            }]
+                    },
+                    treeConfig: {
+                        hideRoot: false
+                    }
+                }
             }
         });
     }
 });
+
